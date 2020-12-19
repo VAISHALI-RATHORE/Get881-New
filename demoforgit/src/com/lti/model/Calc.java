5d430a34d71d9318e736ef30ea1a42a0196b6f83
vaishali <vaishali@192.168.0.2>
2020-12-18 16:06:09 -0800

    first commit

---
+++ demoforgit/src/com/lti/model/Calc.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Calc {
+	public void welcome() {
+		System.out.println("calc is here");
+	}
+	public void add(int num1,int num2) {
+		System.out.println(num1+num2);
+	}
+	public void sub(int num1,int num2) {
+		System.out.println(num1-num2);
+	}
+	
+}
---
+++ demoforgit/src/com/lti/model/Sample.java
@@ -0,0 +1,15 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("hello");
+	}
+	public void method() {
+		System.out.println("to my world");
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
